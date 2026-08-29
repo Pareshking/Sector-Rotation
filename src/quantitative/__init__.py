2daead55bb1aaaac46cc29fb1fd46b26faebef02
Pareshking <pareshking@gmail.com>
2026-08-29 18:31:34 +0530

    feat: add quantitative package

---
+++ src/quantitative/__init__.py
@@ -0,0 +1,5 @@
+from .ranking import rank_exposures
+from .relative_strength import mansfield_relative_strength, rs_stage
+from .returns import dual_momentum, period_return
+
+__all__ = ["dual_momentum", "mansfield_relative_strength", "period_return", "rank_exposures", "rs_stage"]
